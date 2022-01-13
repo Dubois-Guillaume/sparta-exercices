0cac88beca68fd1c05c8284295c17bfcce44e5fa
Guillaume Dubois <duboisguillaume08@gmail.com>
2022-01-13 14:35:20 +0100

    Third exercice

--- 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/03_odd_even/src/index.js
+++ 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/03_odd_even/src/index.js
@@ -1,5 +1,15 @@
-function oddEven() {
+function oddEven(number) {
   // Code the function here.
+  let evenOdd = number % 2;
+  if (Number.isInteger(number)){
+    if (evenOdd !== 0){
+      return `${number} is an odd number.`;
+    } else {
+      return `${number} is an even number.`;
+    }
+  } else {
+    console.log(`${number} is not a number.`);
+  }
 }
 
 // Do not remove last lines, it is for tests

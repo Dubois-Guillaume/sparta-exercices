8742d76ba21c4c2995c9037f288e87772ea3a780
Guillaume Dubois <duboisguillaume08@gmail.com>
2022-01-13 11:42:43 +0100

    Second exercice

--- 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/02_fortune_teller/src/index.js
+++ 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/02_fortune_teller/src/index.js
@@ -1,5 +1,6 @@
-function tellFortune() {
+function tellFortune(job, city, name, years) {
   // Code the function here.
+  console.log(`You will be ${job} in ${city}, and married to ${name} in ${years} years.`);
 }
 
 // Do not remove last lines, it is for tests

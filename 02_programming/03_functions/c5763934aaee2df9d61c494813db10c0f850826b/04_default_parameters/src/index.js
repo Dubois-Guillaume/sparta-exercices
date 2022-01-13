943ae381912822aa295e0732d943584e656cd496
Guillaume Dubois <duboisguillaume08@gmail.com>
2022-01-13 17:08:24 +0100

    Fourth exercice not finished yet

--- 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/04_default_parameters/src/index.js
+++ 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/04_default_parameters/src/index.js
@@ -1,5 +1,12 @@
-function helloSpartan() {
+function helloSpartan(soldier = "Hello soldier; not very talkative uh?") {
   // Code the function here.
+  if ( soldier === "soldier" ){
+    console.log("Hello soldier; not very talkative uh?");
+  } else if ( soldier === String ){
+    console.log(`Hello ${soldier}: glad to know your name!`);
+  } else if ( soldier !== String && soldier !== "soldier" ){
+    console.log("Hello soldier; not very talkative uh?");
+  }
 }
 
 // Do not remove last lines, it is for tests

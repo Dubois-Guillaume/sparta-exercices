f8305142094da0d47570671d71baed981463e565
Guillaume Dubois <duboisguillaume08@gmail.com>
2022-01-13 09:32:57 +0100

    Eighth exercice

--- 02_programming/02_conditions/a136f453a4ff29934baae3372d41abb3f52d90ba/08_object_values/src/index.js
+++ 02_programming/02_conditions/a136f453a4ff29934baae3372d41abb3f52d90ba/08_object_values/src/index.js
@@ -17,13 +17,13 @@ const armouryWithNamesOnly = Object.values(armoury);
 // Create a constant isTheRequestInArmoury bound to the check of the presence
 // of odinRequest in armouryWithNamesOnly
 
-const isTheRequestInArmoury = armouryWithNamesOnly === odinRequest ? true : false;
+const isTheRequestInArmoury = armouryWithNamesOnly.includes(odinRequest);
 
 // Create a condition that logs "Ah, I finally found something to hammer that nail into"
 // if the isTheRequestInArmoury is true and "What do you want me to do with this?" if not
 
-if (isTheRequestInArmoury === true) {
+if (isTheRequestInArmoury) {
   console.log("Ah, I finally found something to hammer that nail into");
-} else if (isTheRequestInArmoury === false) {
+} else if (!isTheRequestInArmoury) {
   console.log("What do you want me to do with this?");
 }

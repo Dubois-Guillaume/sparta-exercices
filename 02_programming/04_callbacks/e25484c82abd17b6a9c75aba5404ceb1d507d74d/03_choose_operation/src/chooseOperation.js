d4a209d1eba86c8540f83a3f1802c89761d75bd0
Guillaume Dubois <duboisguillaume08@gmail.com>
2022-01-14 16:50:45 +0100

    Third exercice not finished

--- 02_programming/04_callbacks/e25484c82abd17b6a9c75aba5404ceb1d507d74d/03_choose_operation/src/chooseOperation.js
+++ 02_programming/04_callbacks/e25484c82abd17b6a9c75aba5404ceb1d507d74d/03_choose_operation/src/chooseOperation.js
@@ -1,20 +1,28 @@
-function addition() {
+function addition(num1, num2) {
   // Code here
+  const Addition = num1 + num2;
+  return Addition;
 }
 
-function substraction() {
+function substraction(num1, num2) {
   // Code here
+  const Substraction = num1 - num2;
+  return Substraction;
 }
 
-function multiplication() {
+function multiplication(num1, num2) {
   // Code here
+  const Multiplication = num1 * num2;
+  return Multiplication;
 }
 
-function division() {
+function division(num1, num2) {
   // Code here
+  const Division = num1 / num2;
+  return Division;
 }
 
-function chooseOperation() {
+function chooseOperation(num1, num2, operator) {
   // Code here
 }
 

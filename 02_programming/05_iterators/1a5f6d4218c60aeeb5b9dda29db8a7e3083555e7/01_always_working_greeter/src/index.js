4f00fe207ec113d162eeb355c96f6750f5d9f6a4
Guillaume Dubois <duboisguillaume08@gmail.com>
2022-01-17 11:04:49 +0100

    First exercice

---
+++ 02_programming/05_iterators/1a5f6d4218c60aeeb5b9dda29db8a7e3083555e7/01_always_working_greeter/src/index.js
@@ -0,0 +1,7 @@
+function greeter(people) {
+  // Code the function here.
+  people.forEach((people) => console.log(`Hello ${people}!`));
+}
+
+// Do not remove the following lines, it is for tests
+module.exports = greeter;

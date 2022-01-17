1c04c3c8e0137757007b3253ba2f7241953e97c2
Guillaume Dubois <duboisguillaume08@gmail.com>
2022-01-17 15:39:00 +0100

    Third exercice

--- 02_programming/05_iterators/1a5f6d4218c60aeeb5b9dda29db8a7e3083555e7/03_every_frame_a_painting/src/index.js
+++ 02_programming/05_iterators/1a5f6d4218c60aeeb5b9dda29db8a7e3083555e7/03_every_frame_a_painting/src/index.js
@@ -1,6 +1,17 @@
-function canTakeArtPieces(pieces) {
-
+function isItPainting(piece) {
+  if (piece.type === "painting"){
+    return true;
+  }else {
+    return false;
+  }
 }
+
+function canTakeArtPieces(pieces) {
+  console.log(pieces);
+  return (pieces.every(isItPainting));
+}
+
+
 
 // Do not remove the following line, it is for tests
 module.exports = canTakeArtPieces;

ae8fbf6ff4e8bb8e118aea3eb45133e02fc00f61
Guillaume Dubois <duboisguillaume08@gmail.com>
2022-01-17 12:04:03 +0100

    Second exercice

--- 02_programming/05_iterators/1a5f6d4218c60aeeb5b9dda29db8a7e3083555e7/02_filtering_letter_e/src/index.js
+++ 02_programming/05_iterators/1a5f6d4218c60aeeb5b9dda29db8a7e3083555e7/02_filtering_letter_e/src/index.js
@@ -1,5 +1,6 @@
 function theEFilter(sentences) {
-
+  const theFilter = sentences.filter(sentences => sentences.includes("e") || sentences.includes("E"));
+  return theFilter;
 }
 
 // Do not remove the following line, it is for tests

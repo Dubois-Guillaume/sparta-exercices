f45dc6d707e0245bf63b1f6475cb92e43116e63d
Guillaume Dubois <duboisguillaume08@gmail.com>
2022-01-18 15:49:54 +0100

    Third exercice

--- 02_programming/06_loops/89d8ffe9d1edbe08b46af9bfaeb92045680be1bd/03_map_with_a_loop/src/index.js
+++ 02_programming/06_loops/89d8ffe9d1edbe08b46af9bfaeb92045680be1bd/03_map_with_a_loop/src/index.js
@@ -1,7 +1,18 @@
-function mapLoop() {
-  // Code the function here.
+// const myVar = [1, 2, 3, 4];
 
+function mapLoop(numbers, double) {
+  // Code the function here.
+  const newNumbers = [];
+
+  for (const each of numbers) {
+    newNumbers.push(double(each));
+  }
+
+  // console.log(newNumbers);
+
+  return newNumbers;
 }
 
+// mapLoop(myVar, (x) => x * 2);
 // Do not remove the following lines, it is for tests
 module.exports = mapLoop;

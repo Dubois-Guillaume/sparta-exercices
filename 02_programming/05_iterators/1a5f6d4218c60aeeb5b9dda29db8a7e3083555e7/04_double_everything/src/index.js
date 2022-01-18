6054c07820d92405ad4adb704affca998a2e3e8e
Guillaume Dubois <duboisguillaume08@gmail.com>
2022-01-18 09:59:30 +0100

    Fourth exercice not finished

--- 02_programming/05_iterators/1a5f6d4218c60aeeb5b9dda29db8a7e3083555e7/04_double_everything/src/index.js
+++ 02_programming/05_iterators/1a5f6d4218c60aeeb5b9dda29db8a7e3083555e7/04_double_everything/src/index.js
@@ -1,5 +1,16 @@
-function double(numbers) {
+function multiply(number) {
+  if (number === 0){
+    return 0;
+  }else {
+    const operator = number * 2;
+    return operator;
+  }
+}
 
+function double(numbers) {
+  const result = numbers.forEach(multiply);
+  console.log(result);
+  return result;
 }
 
 // Do not remove the following line, it is for tests

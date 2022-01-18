a181a8538448aa120e37fa55ccf1e4e019200326
Guillaume Dubois <duboisguillaume08@gmail.com>
2022-01-18 10:16:43 +0100

    First exercice

--- 02_programming/06_loops/89d8ffe9d1edbe08b46af9bfaeb92045680be1bd/01_greeter_for/src/index.js
+++ 02_programming/06_loops/89d8ffe9d1edbe08b46af9bfaeb92045680be1bd/01_greeter_for/src/index.js
@@ -1,6 +1,8 @@
 function greeter(people) {
   // Code the function here.
-
+  for (const name of people) {
+    console.log(`Hello ${name}!`);
+  }
 }
 
 // Do not remove the following lines, it is for tests

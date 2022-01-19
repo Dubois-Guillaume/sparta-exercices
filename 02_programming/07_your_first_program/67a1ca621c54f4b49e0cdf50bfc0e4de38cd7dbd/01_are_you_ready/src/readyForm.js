db2ca4af73449682d5ac9fb30147168457fb6c51
Guillaume Dubois <duboisguillaume08@gmail.com>
2022-01-19 10:29:35 +0100

    First exercice

--- 02_programming/07_your_first_program/67a1ca621c54f4b49e0cdf50bfc0e4de38cd7dbd/01_are_you_ready/src/readyForm.js
+++ 02_programming/07_your_first_program/67a1ca621c54f4b49e0cdf50bfc0e4de38cd7dbd/01_are_you_ready/src/readyForm.js
@@ -1,5 +1,10 @@
 const readyForm = (reader) => {
   // Code here
+  reader.question("Hello user! \n Are you ready for today ??!", (isReady) => {
+    console.log(`WOW, ${isReady} ??!! Really Nice! Let's goo !!.`);
+
+    reader.close();
+  });
 };
 
 // Leave line below for tests to work

58d8d9e9daea1bc556aee961688ef936d5f83126
Guillaume Dubois <duboisguillaume08@gmail.com>
2022-01-19 17:50:17 +0100

    Third exercise  not finished

--- 02_programming/07_your_first_program/67a1ca621c54f4b49e0cdf50bfc0e4de38cd7dbd/03_number_game/src/numberGame.js
+++ 02_programming/07_your_first_program/67a1ca621c54f4b49e0cdf50bfc0e4de38cd7dbd/03_number_game/src/numberGame.js
@@ -1,5 +1,36 @@
+const { check } = require("prettier");
+
 function numberGame(reader, min = 1, max = 100) {
   // code here
+
+  let winNumber = Math.ceil(Math.random() * (max - min) + min);
+  console.log(winNumber); //a commenter pour le test
+
+  const numberQuestion = () =>
+    reader.question("Enter a number: ", (guess) => {
+      console.log(guess.typeOf);
+      function checkingFilter(guess) {
+        console.log(Number.isNaN(guess));
+        if (Number.isNaN(guess) === false) {
+          console.log("This was not a number");
+          numberQuestion();
+        } else if (guess < min || guess > max) {
+          console.log("The number is between 1 and 100");
+          numberQuestion();
+        } else if (guess > winNumber) {
+          console.log("Too high");
+          numberQuestion();
+        } else if (guess < winNumber) {
+          console.log("Too low");
+          numberQuestion();
+        } else if (guess === winNumber) {
+          console.log("You won!");
+          reader.close();
+        }
+      }
+      checkingFilter(guess);
+    });
+  numberQuestion();
 }
 
 module.exports = numberGame;

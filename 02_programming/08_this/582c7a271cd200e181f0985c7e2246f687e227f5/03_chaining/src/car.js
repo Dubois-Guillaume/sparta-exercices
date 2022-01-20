4c1a19a134ba3830a45304032da1b13b082a09d1
Guillaume Dubois <duboisguillaume08@gmail.com>
2022-01-20 17:14:20 +0100

    Third exercise not finished

--- 02_programming/08_this/582c7a271cd200e181f0985c7e2246f687e227f5/03_chaining/src/car.js
+++ 02_programming/08_this/582c7a271cd200e181f0985c7e2246f687e227f5/03_chaining/src/car.js
@@ -1,8 +1,27 @@
 // Complete and add needed car properties:
 const car = {
   speed: 0,
+  driving: 0,
+  distance: 0,
   start: function () {
-  
+    this.speed = 0;
+    this.driving = 0;
+    this.distance = 0;
+    return car;
+  },
+  changeSpeed: function (maxSpeed) {
+    this.speed = maxSpeed;
+    return car;
+  },
+  drive: function (drivingTime) {
+    this.driving = drivingTime / 60;
+    return car;
+  },
+  showDistance: function () {
+    let kM = this.speed * this.driving;
+    this.distance = kM;
+    console.log(`${this.distance} Km`);
+    return car;
   },
 };
 

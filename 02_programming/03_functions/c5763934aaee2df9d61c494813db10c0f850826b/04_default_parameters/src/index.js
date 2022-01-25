4bbfb5fda411b7bf4ebfe227841ca88e849df69c
Guillaume Dubois <duboisguillaume08@gmail.com>
2022-01-25 12:10:22 +0100

    Fourth exercise change default parameter remove a else if

--- 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/04_default_parameters/src/index.js
+++ 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/04_default_parameters/src/index.js
@@ -1,8 +1,6 @@
-function helloSpartan(soldier = "Hello soldier; not very talkative uh?") {
+function helloSpartan(soldier = "soldier") {
   // Code the function here.
-  if ( soldier === "soldier" ){
-    console.log("Hello soldier; not very talkative uh?");
-  } else if ( soldier === String && soldier !== "soldier"){
+  if (soldier !== "soldier") {
     console.log(`Hello ${soldier}: glad to know your name!`);
   } else {
     console.log("Hello soldier; not very talkative uh?");

ca77ecca8b673da833711a5b172557bc68151a44
Guillaume Dubois <duboisguillaume08@gmail.com>
2022-01-25 10:24:06 +0100

    Ninth exercise Capitalized String

--- 02_programming/01_variables/969830e3c3dda891a6b1aeed2709911232022bb1/08_play_with_capitalized_strings/src/index.js
+++ 02_programming/01_variables/969830e3c3dda891a6b1aeed2709911232022bb1/08_play_with_capitalized_strings/src/index.js
@@ -2,3 +2,6 @@ const question = "spartan, what is your profession?!";
 
 // a constant `capitalizedSentence` bound to a string: "Spartan, what is your profession?!"
 // using some of the previous string methods and the constant `question`
+
+const capitalizedSentence = `${question[0].toUpperCase()}${question.slice([1])}`;
+console.log(capitalizedSentence);

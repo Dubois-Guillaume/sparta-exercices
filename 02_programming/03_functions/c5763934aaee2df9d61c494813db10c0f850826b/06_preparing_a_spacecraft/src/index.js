7299aa25bd92e08595d99947c89d691d143cfc10
Guillaume Dubois <duboisguillaume08@gmail.com>
2022-01-26 15:35:44 +0100

    Sixth exercise

--- 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/06_preparing_a_spacecraft/src/index.js
+++ 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/06_preparing_a_spacecraft/src/index.js
@@ -1,9 +1,30 @@
-function checkFuelLevel() {
+function checkFuelLevel(fuelLevel) {
   // Code here
+  let isTheFuelOK;
+  if (fuelLevel >= 20) {
+    isTheFuelOK = "Fuel level: green";
+  } else if (fuelLevel >= 10) {
+    isTheFuelOK = "Fuel level: yellow";
+  } else {
+    isTheFuelOK = "Fuel level: red";
+  }
+  return isTheFuelOK;
 }
 
-function checkCargoHold(){
+function checkCargoHold(cargoCharge) {
   // Code here
+  let cargoMaxCharge = 4;
+  let isTheCargoHoldOK = "";
+  if (cargoCharge.length === 4) {
+    isTheCargoHoldOK = "Full";
+  } else if (cargoCharge.length < 4) {
+    let notEnoughCharge = cargoMaxCharge - cargoCharge.length;
+    isTheCargoHoldOK = `Spaces available: ${notEnoughCharge}`;
+  } else if (cargoCharge.length > 4) {
+    let toManyCharge = cargoCharge.length - cargoMaxCharge;
+    isTheCargoHoldOK = `Over capacity by ${toManyCharge} items.`;
+  }
+  return isTheCargoHoldOK;
 }
 
 // Do not remove last lines, it is for tests

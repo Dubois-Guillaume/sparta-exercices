24160fb53c2f5102a1a194a722d8f1ddd86812d2
Guillaume Dubois <duboisguillaume08@gmail.com>
2022-01-26 16:03:39 +0100

    Seventh exercise

--- 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/07_checking_a_spacecraft/src/index.js
+++ 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/07_checking_a_spacecraft/src/index.js
@@ -1,5 +1,32 @@
 // Code here
 
+const stealthyFuelLevelCheck = (fuelLevel) => {
+  let isTheFuelOK;
+  if (fuelLevel >= 20) {
+    isTheFuelOK = "Fuel level: green";
+  } else if (fuelLevel >= 10) {
+    isTheFuelOK = "Fuel level: yellow";
+  } else {
+    isTheFuelOK = "Fuel level: red";
+  }
+  return isTheFuelOK;
+};
+
+const stealthyCargoHoldCheck = (cargoCharge) => {
+  let cargoMaxCharge = 4;
+  let isTheCargoHoldOK = "";
+  if (cargoCharge.length === 4) {
+    isTheCargoHoldOK = "Full";
+  } else if (cargoCharge.length < 4) {
+    let notEnoughCharge = cargoMaxCharge - cargoCharge.length;
+    isTheCargoHoldOK = `Spaces available: ${notEnoughCharge}`;
+  } else if (cargoCharge.length > 4) {
+    let toManyCharge = cargoCharge.length - cargoMaxCharge;
+    isTheCargoHoldOK = `Over capacity by ${toManyCharge} items.`;
+  }
+  return isTheCargoHoldOK;
+};
+
 // Do not remove last lines, it is for tests
 
 module.exports = { stealthyFuelLevelCheck, stealthyCargoHoldCheck };

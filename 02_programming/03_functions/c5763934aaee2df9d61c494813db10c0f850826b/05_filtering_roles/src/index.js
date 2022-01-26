206ef6ae5b1f915a541431fdc3a8af3871bc86fd
Guillaume Dubois <duboisguillaume08@gmail.com>
2022-01-26 11:01:46 +0100

    Fifth exercise add Array.filter

--- 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/05_filtering_roles/src/index.js
+++ 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/05_filtering_roles/src/index.js
@@ -1,8 +1,17 @@
-function filteredRoles(personArray, personRole = "Teacher") {
+function filteredRoles(person, role = "Teacher") {
   // Code the function here.
   const personLinkToRole = [];
-
-  return personLinkToRole;
+  if (role !== "Student" && role !== "Teacher") {
+    return personLinkToRole;
+  } else {
+    if (role === "Student") {
+      const students = person.filter((person) => person.role === "Student");
+      return students;
+    } else if (role === "Teacher") {
+      const teachers = person.filter((person) => person.role === "Teacher");
+      return teachers;
+    }
+  }
 }
 
 // Do not remove lines below, it is for tests

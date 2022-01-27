f6b517f976372b2e25dd921ee7d76f0d4a2af19a
Guillaume Dubois <duboisguillaume08@gmail.com>
2022-01-27 16:37:56 +0100

    Fifth exercise

--- 02_programming/04_callbacks/0ccf7e82d156e561cf05ec13c2dead0cede4ee4d/05_destructure_and_spread_intermediate/src/index.js
+++ 02_programming/04_callbacks/0ccf7e82d156e561cf05ec13c2dead0cede4ee4d/05_destructure_and_spread_intermediate/src/index.js
@@ -10,8 +10,21 @@ let storageHell = [
 ];
 // Should be cleaned this way => ['IPad', 'IPhone', 'GameBoy color', 'Nes', 'Donkey kong 64', 'Apex Legends Starter Pack', 'LG 5K 27p screen', 'Coffee Machine', 'Azelad']
 
-function formatStorage() {
+function formatStorage(storageArray) {
   // Code your function here
+  let formatedStorage = [];
+  for (let i = 0; i < storageArray.length; i++) {
+    // console.log(17, storageArray[i]);
+    const temporaryArray = storageArray[i];
+    for (let j = 0; j < temporaryArray.length; j++) {
+      // console.log(19, temporaryArray[j]);
+      // const [firstElement] = storageArray;
+      // console.log("19", firstElement);
+      formatedStorage.push(temporaryArray[j]);
+    }
+  }
+  // console.log("22", formatedStorage);
+  return formatedStorage;
 }
 
 // ↓ Feel free to uncomment this next line, or even create more to try everything you do ↓

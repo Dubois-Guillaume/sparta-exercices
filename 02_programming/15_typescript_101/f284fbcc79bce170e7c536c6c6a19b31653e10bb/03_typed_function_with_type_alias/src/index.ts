ad76cfeb3001a8993d43cb95740dd9636b7279f5
Guillaume Dubois <duboisguillaume08@gmail.com>
2022-01-31 12:00:15 +0100

    Third exercise

--- 02_programming/15_typescript_101/f284fbcc79bce170e7c536c6c6a19b31653e10bb/03_typed_function_with_type_alias/src/index.ts
+++ 02_programming/15_typescript_101/f284fbcc79bce170e7c536c6c6a19b31653e10bb/03_typed_function_with_type_alias/src/index.ts
@@ -1,6 +1,13 @@
-function handleTuple() {
+type MyTuple = [string, number];
+
+function handleTuple(tupleArray: MyTuple): void {
   // Code the function here.
+  if (typeof tupleArray[0] === "string" && typeof tupleArray[1] === "number") {
+    console.log(`${tupleArray[0]} is a string`);
+    console.log(`${tupleArray[1]} is a number`);
+  }
 }
 
+// handleTuple(["foo", 1]);
 // Leave the line below for tests to work properly.
 export { handleTuple };

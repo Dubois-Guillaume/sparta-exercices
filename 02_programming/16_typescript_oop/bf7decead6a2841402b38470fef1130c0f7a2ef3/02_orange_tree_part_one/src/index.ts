7ee6839114bfa02c80c44e2feee4d4478eea451b
Guillaume Dubois <duboisguillaume08@gmail.com>
2022-02-01 10:26:24 +0100

    Second exercise orange tree initialization

--- 02_programming/16_typescript_oop/bf7decead6a2841402b38470fef1130c0f7a2ef3/02_orange_tree_part_one/src/index.ts
+++ 02_programming/16_typescript_oop/bf7decead6a2841402b38470fef1130c0f7a2ef3/02_orange_tree_part_one/src/index.ts
@@ -1,4 +1,27 @@
 // Code the class here.
+class Tree {
+  age: number;
+  height: number;
+  alive: boolean = true;
 
+  constructor(age: number) {
+    if (age === 0) {
+      this.height = 0;
+    } else if (age > 0 && age <= 9) {
+      this.height = age * 25;
+    } else if (age >= 10 && age <= 20) {
+      age = age - 9;
+      this.height = 225 + age * 10;
+    } else {
+      this.height = 335;
+    }
+  }
+
+  seed(): void {
+    this.age = 0;
+    this.height = 0;
+    this.alive = true;
+  }
+}
 // Leave the line below for tests to work properly.
 export { Tree };

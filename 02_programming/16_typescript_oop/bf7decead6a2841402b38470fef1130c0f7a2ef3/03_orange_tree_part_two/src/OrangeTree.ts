9ac373b356f508fd317b86d41f48592eec956a41
Guillaume Dubois <duboisguillaume08@gmail.com>
2022-02-02 10:02:30 +0100

    Third exercise connecting class with TypeScript

--- 02_programming/16_typescript_oop/bf7decead6a2841402b38470fef1130c0f7a2ef3/03_orange_tree_part_two/src/OrangeTree.ts
+++ 02_programming/16_typescript_oop/bf7decead6a2841402b38470fef1130c0f7a2ef3/03_orange_tree_part_two/src/OrangeTree.ts
@@ -1,6 +1,43 @@
 import { Tree } from "./Tree";
 
 // Code your class here.
+class OrangeTree extends Tree {
+  oranges: string[] = [];
 
+  constructor(age: number) {
+    super(age);
+  }
+
+  ageOneYear(): void {
+    this.age++;
+    // this.growOranges();
+    if (this.isAlive() === true) {
+      if (this.age > 0 && this.age <= 9) {
+        this.height + 25;
+      } else if (this.age >= 10 && this.age <= 20) {
+        this.height + 10;
+      } else {
+        this.height;
+      }
+    }
+  }
+
+  isAlive(): boolean {
+    let deathChance: number = 0;
+    if (this.age === 100) {
+      this.alive = false;
+    } else if (this.age >= 50 && this.age <= 99) {
+      const randomDeathPercentage: number = Math.ceil(Math.random() * 4);
+      deathChance += randomDeathPercentage;
+      if (deathChance >= 100) {
+        this.alive = false;
+        deathChance = 0;
+      } else {
+        this.alive = true;
+      }
+    }
+    return this.alive;
+  }
+}
 // Leave the line below for tests to work properly.
 export { OrangeTree };
--- 02_programming/16_typescript_oop/bf7decead6a2841402b38470fef1130c0f7a2ef3/03_orange_tree_part_two/src/Tree.ts
+++ 02_programming/16_typescript_oop/bf7decead6a2841402b38470fef1130c0f7a2ef3/03_orange_tree_part_two/src/Tree.ts
@@ -1,4 +1,31 @@
 // Paste your previous `Tree` class here.
+class Tree {
+  age: number;
+  height: number;
+  alive: boolean = true;
 
+  constructor(age: number) {
+    this.age = age;
+    // console.log(this.age);
+    if (age === 0) {
+      this.height = 0;
+    } else if (age > 0 && age <= 9) {
+      this.height = age * 25;
+    } else if (age >= 10 && age <= 20) {
+      age = age - 9;
+      this.height = 225 + age * 10;
+    } else {
+      this.height = 335;
+    }
+  }
+
+  seed(): void {
+    this.age = 0;
+    this.height = 0;
+    this.alive = true;
+  }
+}
+
+// const newTree = new Tree(23);
 // Leave the line below for tests to work properly.
 export { Tree };

6441790a297ba65d6cfa1a6972dc2f7b98036148
Guillaume Dubois <duboisguillaume08@gmail.com>
2022-02-02 14:43:19 +0100

    First exercise Typing(TypeScript) a function

--- 02_programming/17_typescript_generics/607d4b538285006c8eafd921b31409725dd8252e/01_fixing_a_bad_data/src/index.ts
+++ 02_programming/17_typescript_generics/607d4b538285006c8eafd921b31409725dd8252e/01_fixing_a_bad_data/src/index.ts
@@ -1,7 +1,20 @@
-// const receivedObject = {
-//   a: 'somestring',
-//   b: 42
-// }
+const receivedObject = {
+  a: "somestring",
+  b: 42,
+};
 
+type ReceivedObject = {
+  a: string;
+  b: number;
+};
+
+function transformObject(receivedObject: ReceivedObject): (string | number)[] {
+  const returnArray = [];
+  returnArray.push(receivedObject.a);
+  returnArray.push(receivedObject.b);
+  return returnArray;
+}
+
+console.log(transformObject(receivedObject));
 // Leave the line below for tests to work properly.
 export { transformObject };

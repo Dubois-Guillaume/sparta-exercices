f08bf5f3da4f5b9db9dd5b6073cad658ad009182
Guillaume Dubois <duboisguillaume08@gmail.com>
2022-02-02 15:21:59 +0100

    Second exercise return key and value

--- 02_programming/17_typescript_generics/607d4b538285006c8eafd921b31409725dd8252e/02_object_transformer/src/index.ts
+++ 02_programming/17_typescript_generics/607d4b538285006c8eafd921b31409725dd8252e/02_object_transformer/src/index.ts
@@ -1,3 +1,17 @@
+const receivedObject = {
+  productName: "somestring",
+  quantity: 42,
+};
 
+type Product = {
+  productName: string;
+  quantity: number;
+};
+
+function productTransformer(receivedObject: Product): [string, string | number][] {
+  return Object.entries(receivedObject);
+}
+
+console.log(productTransformer(receivedObject));
 // Leave the line below for tests to work properly.
 export { productTransformer };

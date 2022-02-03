f1af2001babc5cfd751c4d02b0ab3674fc00a123
Guillaume Dubois <duboisguillaume08@gmail.com>
2022-02-03 09:32:23 +0100

    Third exercise

--- 02_programming/17_typescript_generics/607d4b538285006c8eafd921b31409725dd8252e/03_become_a_dev/src/index.ts
+++ 02_programming/17_typescript_generics/607d4b538285006c8eafd921b31409725dd8252e/03_become_a_dev/src/index.ts
@@ -1,4 +1,19 @@
+type Human = {
+  name: string;
+  age: number;
+  knowsDev: boolean;
+};
 
+type Dev = Human & {
+  knownLanguage: string;
+};
 
+function becomeADev(name: string, age: number): void {
+  this.Human.name = name;
+  this.human.age = age;
+  this.Human.knowsDev = true;
+  this.Dev.knownLanguage = "JavaScript";
+  return;
+}
 // Leave the line below for tests to work properly.
 export { becomeADev };

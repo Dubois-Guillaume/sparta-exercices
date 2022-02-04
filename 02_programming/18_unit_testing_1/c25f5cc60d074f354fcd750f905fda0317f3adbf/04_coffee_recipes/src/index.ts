e00097872d618d01d303a400e972d69160be2e11
Guillaume Dubois <duboisguillaume08@gmail.com>
2022-02-04 10:50:16 +0100

    Fourth exercise coffee list testing

--- 02_programming/18_unit_testing_1/c25f5cc60d074f354fcd750f905fda0317f3adbf/04_coffee_recipes/src/index.ts
+++ 02_programming/18_unit_testing_1/c25f5cc60d074f354fcd750f905fda0317f3adbf/04_coffee_recipes/src/index.ts
@@ -3,14 +3,23 @@ import { coffeeRecipes, CoffeeRecipe } from "./index.test";
 export function giveRecipe(coffeeName: string): string {
   //code here
   const firstRecipe: CoffeeRecipe = coffeeRecipes[0];
-  let secondRecipe: CoffeeRecipe = coffeeRecipes[1];
-  let thirdRecipe: CoffeeRecipe = coffeeRecipes[2];
-  let fourthRecipe: CoffeeRecipe = coffeeRecipes[3];
-  let fifthRecipe: CoffeeRecipe = coffeeRecipes[4];
-  console.log(firstRecipe);
+  const secondRecipe: CoffeeRecipe = coffeeRecipes[1];
+  const thirdRecipe: CoffeeRecipe = coffeeRecipes[2];
+  const fourthRecipe: CoffeeRecipe = coffeeRecipes[3];
+  const fifthRecipe: CoffeeRecipe = coffeeRecipes[4];
   let recipe: string;
   if (coffeeName === "espresso") {
-    
+    recipe = `${firstRecipe.name} ingredients:\n- ${firstRecipe.ingredients.join(",\n- ")}`;
+  } else if (coffeeName === "macchiato") {
+    recipe = `${secondRecipe.name} ingredients:\n- ${secondRecipe.ingredients.join(",\n- ")}`;
+  } else if (coffeeName === "latte") {
+    recipe = `${thirdRecipe.name} ingredients:\n- ${thirdRecipe.ingredients.join(",\n- ")}`;
+  } else if (coffeeName === "cappuccino") {
+    recipe = `${fourthRecipe.name} ingredients:\n- ${fourthRecipe.ingredients.join(",\n- ")}`;
+  } else if (coffeeName === "americano") {
+    recipe = `${fifthRecipe.name} ingredients:\n- ${fifthRecipe.ingredients.join(",\n- ")}`;
+  } else {
+    recipe = `Sorry, ${coffeeName} does not exist on our list.`;
   }
   return recipe;
 }

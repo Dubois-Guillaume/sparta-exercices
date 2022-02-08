202faa2b1b9246f287b5780656717721dc087625
Guillaume Dubois <duboisguillaume08@gmail.com>
2022-02-08 10:40:38 +0100

    Second exercise

--- 03_frontend_development/02_express/f5c04629aa46be23ca8de8a06fba33f447289dc8/02_warmup_sorting_machine/src/index.ts
+++ 03_frontend_development/02_express/f5c04629aa46be23ca8de8a06fba33f447289dc8/02_warmup_sorting_machine/src/index.ts
@@ -1,1 +1,11 @@
 // Code here
+function sortingMachine(revenues: number[], upOrDown: boolean): number[] {
+  if (upOrDown === true) {
+    revenues.sort((a, b) => a - b);
+  } else {
+    revenues.sort((a, b) => b - a);
+  }
+  return revenues;
+}
+
+export { sortingMachine };

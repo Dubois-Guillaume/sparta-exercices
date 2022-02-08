35da69d8511e35b419c30176a41b464430b08306
Guillaume Dubois <duboisguillaume08@gmail.com>
2022-02-08 10:29:39 +0100

    First exercise

--- 03_frontend_development/02_express/f5c04629aa46be23ca8de8a06fba33f447289dc8/01_warmup_iterate_on_data/src/index.ts
+++ 03_frontend_development/02_express/f5c04629aa46be23ca8de8a06fba33f447289dc8/01_warmup_iterate_on_data/src/index.ts
@@ -1,1 +1,28 @@
 // Code here
+const arrayOfCustomers = [
+  { id: 1, first_name: "Filberto", last_name: "Mowles", email: "fmowles3@msu.local", gender: "Female" },
+  { id: 1, first_name: "Filberto", last_name: "Mowles", email: "fmowles3@msu.local", gender: "Female" },
+  { id: 1, first_name: "Filberto", last_name: "Mowles", email: "fmowles3@msu.local", gender: "Female" },
+  { id: 1, first_name: "Filberto", last_name: "Mowles", email: "fmowles3@msu.local", gender: "Female" },
+];
+type CustomerType = {
+  id: number;
+  first_name: string;
+  last_name: string;
+  email: string;
+  gender: string;
+};
+
+function getEmails(arrayOfCustomers: CustomerType[]): string[] {
+  const emailReturn: string[] = [];
+  for (let i = 0; i < arrayOfCustomers.length; i++) {
+    const arrayIndex = arrayOfCustomers[i];
+    const emailString = arrayIndex.email;
+    emailReturn.push(emailString);
+  }
+  return emailReturn;
+}
+
+console.log(getEmails(arrayOfCustomers));
+
+export { getEmails };

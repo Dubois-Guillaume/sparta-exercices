37ca99d6e98ef03ba521422e77fcb6f6bdf6c361
Guillaume Dubois <duboisguillaume08@gmail.com>
2022-02-09 15:17:48 +0100

    Second exercise

--- 03_frontend_development/03_templating/941283fd0f53524acaead7d02ba27226e7dae5af/02_fundamental_capitalize_context/src/capitalize.ts
+++ 03_frontend_development/03_templating/941283fd0f53524acaead7d02ba27226e7dae5af/02_fundamental_capitalize_context/src/capitalize.ts
@@ -2,4 +2,7 @@
 
 export function capitalize(word: string): string {
   // Code your function here
+  const firstLetter = word.charAt(0).toUpperCase();
+  const otherLetter = word.slice(1, word.length);
+  return `${firstLetter}${otherLetter}`;
 }
--- 03_frontend_development/03_templating/941283fd0f53524acaead7d02ba27226e7dae5af/02_fundamental_capitalize_context/src/context.ts
+++ 03_frontend_development/03_templating/941283fd0f53524acaead7d02ba27226e7dae5af/02_fundamental_capitalize_context/src/context.ts
@@ -1,5 +1,14 @@
-import { capitalize } from "./capitalize"
+import { capitalize } from "./capitalize";
 
 export function context(sentence: string): string {
   // Code here and use the function `capitalize` from exercise one
+  let str = sentence;
+  // console.log(6, str);
+  const words = str.split(" ");
+  // console.log(8, words);
+  const toCapitalize = words.map((word) => capitalize(word));
+  // console.log("boucle", words);
+  const newString = toCapitalize.join(" ");
+  str = newString;
+  return str;
 }

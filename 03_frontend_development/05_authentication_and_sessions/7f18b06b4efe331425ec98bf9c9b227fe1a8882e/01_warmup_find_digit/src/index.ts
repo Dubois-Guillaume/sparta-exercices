c047d10f866cbf7e5a8350e297aae3cb68409bc0
Guillaume Dubois <duboisguillaume08@gmail.com>
2022-02-11 11:35:48 +0100

    First exercise

--- 03_frontend_development/05_authentication_and_sessions/7f18b06b4efe331425ec98bf9c9b227fe1a8882e/01_warmup_find_digit/src/index.ts
+++ 03_frontend_development/05_authentication_and_sessions/7f18b06b4efe331425ec98bf9c9b227fe1a8882e/01_warmup_find_digit/src/index.ts
@@ -1,1 +1,17 @@
 // Code here
+export function findDigit(numberArray: number[], digits: number): number[] {
+  const stringDigit = digits.toString();
+  const result = numberArray.filter((number) => {
+    const stringNumber = number.toString();
+    if (stringNumber.includes(stringDigit)) {
+      return true;
+    } else {
+      return false;
+    }
+  });
+  console.log(result);
+  return result;
+}
+
+const anArrayOfNumbers: number[] = [1, 92, 824];
+findDigit(anArrayOfNumbers, 1);

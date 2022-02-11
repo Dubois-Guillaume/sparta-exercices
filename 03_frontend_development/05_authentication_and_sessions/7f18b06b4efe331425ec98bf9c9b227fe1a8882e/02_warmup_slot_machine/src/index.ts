c36018a456aaa8ea3371b2690df8802853abb8bd
Guillaume Dubois <duboisguillaume08@gmail.com>
2022-02-11 11:53:54 +0100

    Second exercise

--- 03_frontend_development/05_authentication_and_sessions/7f18b06b4efe331425ec98bf9c9b227fe1a8882e/02_warmup_slot_machine/src/index.ts
+++ 03_frontend_development/05_authentication_and_sessions/7f18b06b4efe331425ec98bf9c9b227fe1a8882e/02_warmup_slot_machine/src/index.ts
@@ -1,1 +1,8 @@
 // Code here
+export function slotMachine(symbolsTuple: [unknown, unknown, unknown]): boolean {
+  if (symbolsTuple[0] === symbolsTuple[1] && symbolsTuple[1] === symbolsTuple[2]) {
+    return true;
+  } else {
+    return false;
+  }
+}

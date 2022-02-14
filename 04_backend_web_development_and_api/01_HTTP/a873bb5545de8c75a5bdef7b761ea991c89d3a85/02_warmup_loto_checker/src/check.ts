7bf857724411774b198d08c09c4d34422284b1c8
Guillaume Dubois <duboisguillaume08@gmail.com>
2022-02-14 11:48:41 +0100

    Second exercise

--- 04_backend_web_development_and_api/01_HTTP/a873bb5545de8c75a5bdef7b761ea991c89d3a85/02_warmup_loto_checker/src/check.ts
+++ 04_backend_web_development_and_api/01_HTTP/a873bb5545de8c75a5bdef7b761ea991c89d3a85/02_warmup_loto_checker/src/check.ts
@@ -1,6 +1,25 @@
-import { lottery_draw } from "./lottery_draw"
+import { lottery_draw } from "./lottery_draw";
 import { Draw } from "./types";
 
 export function checkIfIWon(ticket: Draw): void {
   // Code the function here
+  const randomTicket = lottery_draw();
+  console.log("tickets", randomTicket, ticket);
+  const digit1 = ticket[0];
+  const digit2 = ticket[1];
+  const digit3 = ticket[2];
+  const randomDigit1 = randomTicket[0];
+  const randomDigit2 = randomTicket[1];
+  const randomDigit3 = randomTicket[2];
+
+  if (digit1 === randomDigit1 && digit2 === randomDigit2 && digit3 === randomDigit3) {
+    console.log("You won the lottery!");
+  } else {
+    console.log("You lost...");
+  }
 }
+
+// checkIfIWon([0, 0, 0]);
+// checkIfIWon([1, 1, 1]);
+// checkIfIWon([2, 2, 2]);
+// checkIfIWon([3, 3, 3]);
--- 04_backend_web_development_and_api/01_HTTP/a873bb5545de8c75a5bdef7b761ea991c89d3a85/02_warmup_loto_checker/src/lottery_draw.ts
+++ 04_backend_web_development_and_api/01_HTTP/a873bb5545de8c75a5bdef7b761ea991c89d3a85/02_warmup_loto_checker/src/lottery_draw.ts
@@ -1,5 +1,10 @@
 import { Draw } from "./types";
 
 export function lottery_draw(): Draw {
-
+  const randomDraw: number[] = [];
+  for (let i = 0; i < 3; i++) {
+    const randomNumber = Math.ceil(Math.random() * 3);
+    randomDraw.push(randomNumber);
+  }
+  return randomDraw;
 }

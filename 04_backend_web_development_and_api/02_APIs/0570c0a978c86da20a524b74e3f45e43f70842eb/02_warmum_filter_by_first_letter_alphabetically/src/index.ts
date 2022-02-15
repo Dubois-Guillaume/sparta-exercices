e3a046d4dabb51e08da957a80d74a13c2ea73ec1
Guillaume Dubois <duboisguillaume08@gmail.com>
2022-02-15 15:20:01 +0100

    Second exercise

--- 04_backend_web_development_and_api/02_APIs/0570c0a978c86da20a524b74e3f45e43f70842eb/02_warmum_filter_by_first_letter_alphabetically/src/index.ts
+++ 04_backend_web_development_and_api/02_APIs/0570c0a978c86da20a524b74e3f45e43f70842eb/02_warmum_filter_by_first_letter_alphabetically/src/index.ts
@@ -1,1 +1,34 @@
 // code here
+import { Customer } from "../data/customers";
+
+export function filterByFirstLetter(customersList: Customer[], letter: string): Customer[] {
+  // Code here
+  const filteredList = customersList.filter((name) => {
+    const firstLetter = name.last_name[0];
+    if (firstLetter === letter) {
+      return true;
+    } else {
+      return false;
+    }
+  });
+  return filteredList;
+}
+
+export function filterByFirstLetterOrdered(customersList: Customer[], letter: string): Customer[] {
+  if (letter.length > 1) {
+    const filteredArray: Customer[] = [];
+    return filteredArray;
+  } else {
+    const customersListFilter = filterByFirstLetter(customersList, letter);
+    const customersListFilterSorted = customersListFilter.sort(function (a, b) {
+      if (a.last_name < b.last_name) {
+        return -1;
+      } else if (a.last_name > b.last_name) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+    return customersListFilterSorted;
+  }
+}

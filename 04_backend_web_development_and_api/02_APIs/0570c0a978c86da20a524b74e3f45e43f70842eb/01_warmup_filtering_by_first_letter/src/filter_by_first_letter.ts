69d4265effdd11f649361c442916c05bf2f4a63a
Guillaume Dubois <duboisguillaume08@gmail.com>
2022-02-15 10:05:44 +0100

    First exercise

--- 04_backend_web_development_and_api/02_APIs/0570c0a978c86da20a524b74e3f45e43f70842eb/01_warmup_filtering_by_first_letter/src/filter_by_first_letter.ts
+++ 04_backend_web_development_and_api/02_APIs/0570c0a978c86da20a524b74e3f45e43f70842eb/01_warmup_filtering_by_first_letter/src/filter_by_first_letter.ts
@@ -1,5 +1,14 @@
 import { Customer } from "../data/customers";
 
-export function filterByFirstLetter() {
+export function filterByFirstLetter(customersList: Customer[], letter: string): Customer[] {
   // Code here
+  const filteredList = customersList.filter((name) => {
+    const firstLetter = name.last_name[0];
+    if (firstLetter === letter) {
+      return true;
+    } else {
+      return false;
+    }
+  });
+  return filteredList;
 }

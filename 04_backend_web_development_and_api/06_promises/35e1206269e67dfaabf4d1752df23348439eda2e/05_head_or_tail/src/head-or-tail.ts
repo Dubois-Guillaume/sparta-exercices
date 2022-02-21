8f1b10ec9cf51b92f889bc328e4e44321f598c1a
Guillaume Dubois <duboisguillaume08@gmail.com>
2022-02-21 15:54:08 +0100

    Fifth exercise head or tail promises

--- 04_backend_web_development_and_api/06_promises/35e1206269e67dfaabf4d1752df23348439eda2e/05_head_or_tail/src/head-or-tail.ts
+++ 04_backend_web_development_and_api/06_promises/35e1206269e67dfaabf4d1752df23348439eda2e/05_head_or_tail/src/head-or-tail.ts
@@ -1,7 +1,16 @@
 import { getRandomFace } from "./random-face";
 
-function headOrTail() {
-
+function headOrTail(guess: string): Promise<string> {
+  return new Promise((resolve, reject) => {
+    const randomFace = getRandomFace();
+    if (guess === "head" || guess === "tail") {
+      if (guess === randomFace) {
+        resolve("Well done.");
+      } else {
+        reject("Nope.");
+      }
+    }
+  });
 }
 
 export { headOrTail };

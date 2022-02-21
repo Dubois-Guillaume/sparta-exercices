eb5e374f202a8a5b0d912ab315b4cb48c39a341e
Guillaume Dubois <duboisguillaume08@gmail.com>
2022-02-21 10:05:03 +0100

    First exercise rest parameters

--- 04_backend_web_development_and_api/06_promises/35e1206269e67dfaabf4d1752df23348439eda2e/01_warmup_greeter_function/src/greetEveryone.ts
+++ 04_backend_web_development_and_api/06_promises/35e1206269e67dfaabf4d1752df23348439eda2e/01_warmup_greeter_function/src/greetEveryone.ts
@@ -1,5 +1,6 @@
 type ParamRest = string[];
 
-export function greetEveryone(): void {
+export function greetEveryone(...names: ParamRest): void {
   // Code your function here
+  return names.forEach((name) => console.log(`Welcome to ${name}`));
 }

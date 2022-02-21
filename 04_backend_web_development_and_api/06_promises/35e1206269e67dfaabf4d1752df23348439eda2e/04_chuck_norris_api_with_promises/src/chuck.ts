4c548fefbf1935f11094572c73713d4cbf55f59f
Guillaume Dubois <duboisguillaume08@gmail.com>
2022-02-21 15:46:46 +0100

    Fourth exercise Chuck Norris Promises

--- 04_backend_web_development_and_api/06_promises/35e1206269e67dfaabf4d1752df23348439eda2e/04_chuck_norris_api_with_promises/src/chuck.ts
+++ 04_backend_web_development_and_api/06_promises/35e1206269e67dfaabf4d1752df23348439eda2e/04_chuck_norris_api_with_promises/src/chuck.ts
@@ -1,9 +1,21 @@
-function getCategories() {
+import fetch from "node-fetch";
+
+function getCategories(): Promise<string[]> {
   // Your code goes here
+  return new Promise((resolve, reject) => {
+    fetch("https://api.chucknorris.io/jokes/categories")
+      .then((response) => response.json())
+      .then((categories) => resolve(categories));
+  });
 }
 
-function getJoke() {
+function getJoke(category: string): Promise<string> {
   // Your code goes here
+  return new Promise((resolve, reject) => {
+    fetch(`https://api.chucknorris.io/jokes/random?category=${category}`)
+      .then((response) => response.json())
+      .then((joke) => resolve(joke.value));
+  });
 }
 
 // Leave the line below for tests to work properly

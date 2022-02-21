5eccd13f1b25e6ad1fa9a2f2cbbc8edb353b8b30
Guillaume Dubois <duboisguillaume08@gmail.com>
2022-02-21 15:13:22 +0100

    Third exercise setTimeOut promise

--- 04_backend_web_development_and_api/06_promises/35e1206269e67dfaabf4d1752df23348439eda2e/03_promisified_settimeout/src/index.ts
+++ 04_backend_web_development_and_api/06_promises/35e1206269e67dfaabf4d1752df23348439eda2e/03_promisified_settimeout/src/index.ts
@@ -1,5 +1,11 @@
 function waitFor(time: number): Promise<void> {
-
+  return new Promise((resolve, reject) => {
+    setTimeout(() => resolve(), time);
+  });
 }
+
+waitFor(2000).then(() => {
+  console.log("This will be printed after 2 seconds");
+});
 
 export { waitFor };

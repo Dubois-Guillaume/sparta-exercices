09c0eae4ad3994778820e7045ee20da9771125c2
Guillaume Dubois <duboisguillaume08@gmail.com>
2022-02-22 12:08:27 +0100

    First exercise

--- 04_backend_web_development_and_api/07_async_await/ba187c73364264f34257148891d6b7aead12ec16/01_warmup_assemble_data/src/assembleData.ts
+++ 04_backend_web_development_and_api/07_async_await/ba187c73364264f34257148891d6b7aead12ec16/01_warmup_assemble_data/src/assembleData.ts
@@ -5,6 +5,25 @@ type Profile = {
   haveTraveled: boolean;
 };
 
-export function assembleData() {
+export function assembleData(names: string[], ages: number[], countries: string[], haveTraveled: boolean[]): Profile[] {
   // Code here
+  const profile: Profile[] = [];
+  for (let i = 0; i < names.length; i++) {
+    const person: Profile = {};
+    person.name = names[i];
+    person.age = ages[i];
+    person.country = countries[i];
+    person.haveTraveled = haveTraveled[i];
+    profile.push(person);
+  }
+  return profile;
 }
+
+//on as des tableau remplis de valeur ( noms, ages, pays et voyageur ou non )
+//on doit prendre un éléments de chaque tableau pour le placer en valeur d'une clé d'un des objets
+//on doit renvoyer un tableau d'objet de type Profile ({
+//  name:"LeNom"
+//  age: "SonAge"
+//  country: "SonPays"
+//  haveTraveled: "SiIlADejaVoyager"
+//})

524f4fa65f7af2375ca257f37b6e6a1120f209b7
Guillaume Dubois <duboisguillaume08@gmail.com>
2022-02-23 11:59:48 +0100

    First exercise

--- 04_backend_web_development_and_api/08_mongoDB_1/a85a90546949642a07834077171af4df55bcfa7f/01_warmup_parse_name/src/parse-name.ts
+++ 04_backend_web_development_and_api/08_mongoDB_1/a85a90546949642a07834077171af4df55bcfa7f/01_warmup_parse_name/src/parse-name.ts
@@ -1,7 +1,12 @@
 type Person = {
   firstName: string;
   lastName: string;
-}
+};
 
 export function parseName(name: string): Person {
+  const splitedName = name.split(" ");
+  const person: Person = {};
+  person.firstName = splitedName[0];
+  person.lastName = splitedName[1];
+  return person;
 }

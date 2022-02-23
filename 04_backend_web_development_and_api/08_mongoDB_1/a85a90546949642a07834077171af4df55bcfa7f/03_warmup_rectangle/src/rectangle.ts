53518a2747e3177a0c0274240080924f12b2a0f7
Guillaume Dubois <duboisguillaume08@gmail.com>
2022-02-23 15:03:33 +0100

    Third exercise

--- 04_backend_web_development_and_api/08_mongoDB_1/a85a90546949642a07834077171af4df55bcfa7f/03_warmup_rectangle/src/rectangle.ts
+++ 04_backend_web_development_and_api/08_mongoDB_1/a85a90546949642a07834077171af4df55bcfa7f/03_warmup_rectangle/src/rectangle.ts
@@ -1,2 +1,10 @@
 export function rectangle(width: number, height: number): string {
+  let rectangle = "";
+  for (let j = 0; j < height; j++) {
+    for (let i = 0; i < width; i++) {
+      rectangle += "*";
+    }
+    rectangle += "\n";
+  }
+  return rectangle;
 }

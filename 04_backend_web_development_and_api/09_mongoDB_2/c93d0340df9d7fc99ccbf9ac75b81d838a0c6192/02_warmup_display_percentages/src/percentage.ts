bd9ae0858c66fbc468f47593d512b7844df81903
Guillaume Dubois <duboisguillaume08@gmail.com>
2022-02-24 11:30:17 +0100

    Second exercise

--- 04_backend_web_development_and_api/09_mongoDB_2/c93d0340df9d7fc99ccbf9ac75b81d838a0c6192/02_warmup_display_percentages/src/percentage.ts
+++ 04_backend_web_development_and_api/09_mongoDB_2/c93d0340df9d7fc99ccbf9ac75b81d838a0c6192/02_warmup_display_percentages/src/percentage.ts
@@ -1,2 +1,11 @@
 export function humanPercentage(percentages: number[]): string[] {
+  const percentageHuman = percentages.map((percentage) => Math.round(percentage * 100) / 100);
+  const stringNumbers = percentageHuman.map((numbers) => numbers.toString().substring(2));
+  return stringNumbers.map((number) => {
+    if (number.length === 1) {
+      return number + "0%";
+    } else {
+      return number + "%";
+    }
+  });
 }

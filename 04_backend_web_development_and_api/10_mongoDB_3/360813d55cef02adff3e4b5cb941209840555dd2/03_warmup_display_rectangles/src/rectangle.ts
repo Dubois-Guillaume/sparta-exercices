aa9fc53f9bae9fabeddd4b248d5a82d613928343
Guillaume Dubois <duboisguillaume08@gmail.com>
2022-02-25 11:09:14 +0100

    Third exercise

--- 04_backend_web_development_and_api/10_mongoDB_3/360813d55cef02adff3e4b5cb941209840555dd2/03_warmup_display_rectangles/src/rectangle.ts
+++ 04_backend_web_development_and_api/10_mongoDB_3/360813d55cef02adff3e4b5cb941209840555dd2/03_warmup_display_rectangles/src/rectangle.ts
@@ -2,5 +2,7 @@ import { line } from "./line";
 import { column } from "./column";
 
 export function rectangle(width: number, height: number): string {
-
+  const large = line(width);
+  const high = column(height, large);
+  return high;
 }

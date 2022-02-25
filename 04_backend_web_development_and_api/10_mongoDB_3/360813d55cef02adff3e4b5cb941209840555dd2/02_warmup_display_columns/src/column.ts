6548bf28714c3d8e6e3884584bcf13075fad0010
Guillaume Dubois <duboisguillaume08@gmail.com>
2022-02-25 10:46:12 +0100

    Second exercise

--- 04_backend_web_development_and_api/10_mongoDB_3/360813d55cef02adff3e4b5cb941209840555dd2/02_warmup_display_columns/src/column.ts
+++ 04_backend_web_development_and_api/10_mongoDB_3/360813d55cef02adff3e4b5cb941209840555dd2/02_warmup_display_columns/src/column.ts
@@ -1,2 +1,7 @@
 export function column(numberOfLines: number, lineContent: string): string {
+  let characters = "";
+  for (let i = 0; i < numberOfLines; i++) {
+    characters += `${lineContent}\n`;
+  }
+  return characters;
 }

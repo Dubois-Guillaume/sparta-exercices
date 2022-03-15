d8e72e105c840ed4b98b0394c990693624fbfe30
Guillaume Dubois <duboisguillaume08@gmail.com>
2022-03-15 09:57:01 +0100

    React useState

--- 06_fullstack_web_development/01_react_functional_components/fe2008a224b48c8983a2d34ead9d860fe99af1ed/01_basic_component/src/components/HideOrNotHide.jsx
+++ 06_fullstack_web_development/01_react_functional_components/fe2008a224b48c8983a2d34ead9d860fe99af1ed/01_basic_component/src/components/HideOrNotHide.jsx
@@ -2,4 +2,11 @@ import React from "react";
 
 export const HideOrNotHide = () => {
   // Code here
+  const [paragraph, setParagraph] = React.useState();
+
+  return (
+    <div>
+      <button onClick={() => setParagraph("bonjour")}>{paragraph}</button>
+    </div>
+  );
 };

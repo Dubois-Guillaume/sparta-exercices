baf2d490c147409c9f04faefb42f1d14f8778c4b
Guillaume Dubois <duboisguillaume08@gmail.com>
2022-03-19 17:38:15 +0100

    commit

--- 06_fullstack_web_development/01_react_functional_components/fe2008a224b48c8983a2d34ead9d860fe99af1ed/02_product_card/src/components/product-card/index.jsx
+++ 06_fullstack_web_development/01_react_functional_components/fe2008a224b48c8983a2d34ead9d860fe99af1ed/02_product_card/src/components/product-card/index.jsx
@@ -1,7 +1,29 @@
 import React from "react";
 
-const ProductCard = () => {
-  return <div />;
+const ProductCard = ({ product }) => {
+  const [flashProduct, setProduct] = React.useState(true);
+  return (
+    <div>
+      <button onClick={() => setProduct(!flashProduct)}>
+        {flashProduct ? product[0].question : product[0].reponse}
+      </button>
+    </div>
+  );
 };
+
+// function componentParent() {
+//   const nom = "vielvoye";
+//   const prenom = "martin";
+
+//   const nomComplet = componentEnfant(nom, prenom)
+
+//   return nomComplet
+// }
+
+// // ====================================
+
+// function componentEnfant(nom, prenom) {
+//   return nom + prenom;
+// }
 
 export default ProductCard;
